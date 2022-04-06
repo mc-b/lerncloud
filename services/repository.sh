fa9aedc90ae091f153e46f54012e75f73ac08406
mc-b <marcel.bernet@ch-open.ch>
2022-04-06 10:56:53 +0200

    Einbindung alte LernMAAS Scripts

--- services/repository.sh
+++ services/repository.sh
@@ -9,6 +9,12 @@ repo=$(basename $1)
 
 if  [ -d ${repo} ]
 then
+
     cd ${repo}
     bash -x scripts/install.sh
+    
+    # Dateien fuer Willkommenseite aufbereiten
+    [ -f README.md ] && [ cp README.md /home/ubuntu/; ]
+    [ -f ACCESSING.md ] && [ cp ACCESSING.md /home/ubuntu/; ]
+    [ -f SERVICES.md ] && [ cp SERVICES.md /home/ubuntu/; ]
 fi
