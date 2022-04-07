df2cc29ad07f6f9fbb86bfbc8ac4c20253194cbd
mc-b <marcel.bernet@ch-open.ch>
2022-04-07 10:23:51 +0200

    Terraform Multipass Modul

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,6 +4,7 @@ module "lerncloud" {
   #source     = "git::https://github.com/mc-b/terraform_lerncloud_aws.git"
   #source     = "git::https://github.com/mc-b/terraform_lerncloud_azure.git"
   #source     = "git::https://github.com/mc-b/terraform_lerncloud_maas.git"
+  #source     = "git::https://github.com/mc-b/terraform_lerncloud_multipass.git"
   module     = "base"
   userdata   = "../modules/base.yaml"
 }
