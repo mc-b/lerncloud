b040b3fc58dece9a436632bf9ce0adb2e5c39fa3
mc-b <marcel.bernet@ch-open.ch>
2022-04-20 10:28:06 +0200

    Vereinheitlichung Variablen und Outputs

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,26 @@
-
+###
+# Basis VM
 
 module "lerncloud" {
-  source     = "git::https://github.com/mc-b/terraform_lerncloud_multipass.git"
-  #source     = "git::https://github.com/mc-b/terraform_lerncloud_aws.git"
-  #source     = "git::https://github.com/mc-b/terraform_lerncloud_azure.git"
-  #source     = "git::https://github.com/mc-b/terraform_lerncloud_maas.git"
-  module     = "base"
-  userdata   = "../modules/base.yaml"
+  source     = "git::https://github.com/mc-b/terraform-lerncloud-maas"
+  #source     = "git::https://github.com/mc-b/terraform-lerncloud-multipass"
+  #source     = "git::https://github.com/mc-b/terraform-lerncloud-aws"
+  #source     = "git::https://github.com/mc-b/terraform-lerncloud-azure"
+
+  # Module Info
+  module      = "base-11"
+  description = "Basis Modul"
+  userdata    = "../modules/base.yaml"
+
+  # VM Sizes  
+  #memory   = 2
+  #cores    = 1
+  #storage  = 32
+  #ports    = [ 22, 80 ]
+
+  # Server Access Info
+  url      = "http://10.6.37.8:5240/MAAS"
+  key      = "${var.key}"
+  vpn      = "10-6-37-0"
 }
+
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,11 +3,16 @@
 #  
 
 output "ip_vm" {
-  value = module.lerncloud.ip_vm
+  value       = module.lerncloud.ip_vm
   description = "The IP address of the server instance."
 }
 
 output "fqdn_vm" {
-  value = module.lerncloud.fqdn_vm
+  value       = module.lerncloud.fqdn_vm
   description = "The FQDN of the server instance."
+}
+
+output "description" {
+  value       = module.lerncloud.description
+  description = "Description VM"
 }
