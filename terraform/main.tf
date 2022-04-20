28ce5601f760db7e5f12ce77d1af4997330f6539
mc-b <marcel.bernet@ch-open.ch>
2022-04-20 18:38:06 +0200

    lernmaas Module

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,6 +3,7 @@
 
 module "lerncloud" {
   source     = "git::https://github.com/mc-b/terraform-lerncloud-maas"
+  #source     = "git::https://github.com/mc-b/terraform-lerncloud-lernmaas"    
   #source     = "git::https://github.com/mc-b/terraform-lerncloud-multipass"
   #source     = "git::https://github.com/mc-b/terraform-lerncloud-aws"
   #source     = "git::https://github.com/mc-b/terraform-lerncloud-azure"
