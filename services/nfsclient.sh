05ead6f1db0a512d1af8685639c2023ed53d0845
mc-b <marcel.bernet@ch-open.ch>
2022-04-25 14:01:26 +0200

    falsches paket

--- services/nfsclient.sh
+++ services/nfsclient.sh
@@ -3,7 +3,7 @@
 #   Installiert den NFS Client und erzeugt /data Verzeichnis
 
 sudo apt-get update
-sudo apt install -y nfs-kernel-common
+sudo apt install -y nfs-common
 
 sudo mkdir -p /data 
 sudo chown -R ubuntu:ubuntu /data
