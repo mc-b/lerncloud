ccced6bff3b397477986ca6c1d3a688d3b73c375
mc-b <marcel.bernet@ch-open.ch>
2022-05-24 16:46:33 +0200

    Ubuntu 20.x

--- services/k8sbase.sh
+++ services/k8sbase.sh
@@ -11,6 +11,6 @@ cat /etc/fstab | grep -v swap.img | sudo tee /etc/fstab
 sudo rm -f /swap.img
 
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-sudo apt-add-repository -y "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+sudo apt-add-repository -y "deb http://apt.kubernetes.io/ kubernetes-bionic main"
 sudo apt-get -q 2 update
 sudo apt-get install -q 2 -y kubelet=${VERSION} kubeadm=${VERSION}
