5664f2449a656b183cbaedfa6b578afb43167d1d
mc-b <marcel.bernet@ch-open.ch>
2022-06-03 18:07:29 +0200

    fehlte

---
+++ wireguard/variables.tf
@@ -0,0 +1,25 @@
+# Zugriffs Informationen
+#
+# Als Umgebungsvariablen TF_VAR_<name> ablegen
+# TF_VAR_url=https://10.6.37.8:5240/MAAS
+
+variable "url" {
+  description = "Evtl. URL fuer den Zugriff auf das API des Racks Servers"
+  type        = string
+  default     = "unknown"
+}
+
+# Umgebungsvariable TF_VAR_key ablegen
+variable "key" {
+  description = "API Key, Token etc. fuer Zugriff"
+  type        = string
+  sensitive   = true
+  default     = "unknown"
+}
+
+variable "vpn" {
+  description = "Optional VPN welches eingerichtet werden soll"
+  type        = string
+  default     = "unknown"
+}
+
