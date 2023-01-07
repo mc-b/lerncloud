1579a6ac87a80acb56c6ef9c7be17ce98b38a4ab
mc-b <marcel.bernet@ch-open.ch>
2023-01-07 15:09:23 +0100

    Container Tools wie buildah, podman

---
+++ services/containers.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#   
+#   Installiert Container Tools wie PodMan, Buildah etc.
+#   @see https://github.com/containers
+#
+
+source /etc/os-release
+
+# Ubuntu 20
+if [ "${VERSION_CODENAME}" == "focal" ]
+then
+    sh -c "echo 'deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_18.04/ /' | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list"
+    wget -nv https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable/xUbuntu_18.04/Release.key -O /tmp/Release.key
+    sudo apt-key add - </tmp/Release.key
+    sudo apt-get update -qq
+    sudo apt-get -qq -y install podman buildah skopeo 
+    sudo apt-get -qq -y install fuse-overlayfs
+
+# Containers Tools (ab Ubuntu 22.x)
+else
+    sudo apt-get update -qq
+    sudo apt-get install -y podman buildah skopeo
+fi
+    
