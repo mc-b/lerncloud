5e2f52757c3f06510c080c16bc1c8674dbae003b
mc-b <marcel.bernet@ch-open.ch>
2023-05-08 08:47:08 +0200

    Neu inkl. Mount Rackserver Folders

--- services/nfsclient.sh
+++ services/nfsclient.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #   
 #   Installiert den NFS Client und erzeugt /data Verzeichnis
+#   Mountet evtl. vorhandene Server Folders
+
+SERVER_IP=$(sudo cat /var/lib/cloud/instance/datasource | cut -d: -f3 | cut -d/ -f3)
 
 sudo apt-get update
 sudo apt-get install -y nfs-common
@@ -12,3 +15,21 @@ sudo chmod 777 /data
 
 # Kompatibilitaet storage.sh
 sudo ln -s /data /home/ubuntu/data
+
+if  [ "${SERVER_IP}" != "" ]
+then
+    sudo mount -t nfs ${SERVER_IP}:/data/config /home/ubuntu/config
+    sudo mount -t nfs ${SERVER_IP}:/data/templates /home/ubuntu/templates
+    sudo mount -t nfs ${SERVER_IP}:/data/storage /home/ubuntu/data
+    
+    # remount data mit neuem Verzeichnis data/${HOSTNAME}
+    sudo mkdir -p /home/ubuntu/data/${HOSTNAME} && chown ubuntu:ubuntu /home/ubuntu/data/${HOSTNAME} && chmod 777 /home/ubuntu/data/${HOSTNAME}
+    sudo umount /home/ubuntu/data
+    sudo mount -t nfs ${SERVER_IP}:/data/storage/${HOSTNAME} /home/ubuntu/data
+    
+    cat <<%EOF% | sudo tee -a /etc/fstab
+${SERVER_IP}:/data/templates            /home/ubuntu/templates  nfs defaults    0 11
+${SERVER_IP}:/data/storage/${HOSTNAME}  /home/ubuntu/data       nfs defaults    0 12
+%EOF%
+
+fi
