6009e2b2fd60274b70f9b4afec85a3c3af53cc12
mc-b <marcel.bernet@ch-open.ch>
2023-05-09 15:40:54 +0200

    nur Aktivieren wenn unterstuetzt

--- services/wake-on-lan.sh
+++ services/wake-on-lan.sh
@@ -3,13 +3,19 @@
 #   Aktiviert Wake on LAN 
 #
 
+trap '' 1 3 9
+
 sudo apt-get -y update
 sudo apt-get install -y ethtool etherwake 
 
-export ETH=$(ip link | awk -F: '$0 !~ "lo|vir|wl|tap|br|wg|docker0|^[^0-9]"{print $2;getline}')
-export ETH=$(echo $ETH | sed 's/ *$//g')
+export ETH=$(ls -1 /sys/class/net/ | grep en)
 
-cat <<EOF | sudo tee /etc/systemd/system/wol.service
+# nur Aktivieren wenn unterstuetzt
+/sbin/ethtool -s ${ETH} wol g
+if  [ $? -eq 0 ]
+then
+
+    cat <<EOF | sudo tee /etc/systemd/system/wol.service
 [Unit]
 Description=Configure Wake-up on LAN
 After=network-online.target
@@ -22,7 +28,7 @@ ExecStart=/sbin/ethtool -s ${ETH} wol g
 WantedBy=basic.target 
 EOF
 
-sudo systemctl enable wol.service 
-sudo systemctl daemon-reload 
-sudo systemctl start wol.service 
-
+    sudo systemctl enable wol.service 
+    sudo systemctl daemon-reload 
+    sudo systemctl start wol.service 
+fi
