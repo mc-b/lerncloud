2ed37cd15b417897776f7335fdb3bbb455539f27
mc-b <marcel.bernet@ch-open.ch>
2023-05-15 11:37:23 +0200

    Optimiert

--- scripts/webvirtcloud.sh
+++ scripts/webvirtcloud.sh
@@ -5,8 +5,8 @@
 
 sudo apt-get -y install git virtualenv python3-virtualenv python3-dev python3-lxml libvirt-dev zlib1g-dev libxslt1-dev nginx supervisor libsasl2-modules gcc pkg-config python3-guestfs libsasl2-dev libldap2-dev libssl-dev
 
-git clone https://github.com/retspen/webvirtcloud
-cd webvirtcloud
+git clone https://github.com/retspen/webvirtcloud /srv/webvirtcloud
+cd /srv/webvirtcloud
 cp webvirtcloud/settings.py.template webvirtcloud/settings.py
 
 cat <<EOF | python3 >secret_key
@@ -19,19 +19,15 @@ rm secret_key
 
 sudo cp conf/supervisor/webvirtcloud.conf /etc/supervisor/conf.d
 sudo cp conf/nginx/webvirtcloud.conf /etc/nginx/conf.d
-cd ..
-sudo mv webvirtcloud /srv
-sudo chown -R ubuntu:ubuntu /srv/webvirtcloud
 
-cd /srv/webvirtcloud
 virtualenv -p python3 venv
 source venv/bin/activate
 pip install -r conf/requirements.txt
 python3 manage.py migrate
 python3 manage.py collectstatic --noinput
+
 sudo chown -R www-data:www-data /srv/webvirtcloud
 sudo rm -f /etc/nginx/sites-enabled/default
-sudo chown -R www-data:www-data /srv/webvirtcloud
 
 # Standard SSH Key 
 sudo mkdir /var/www/.ssh
