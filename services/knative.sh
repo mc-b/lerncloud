c634232ecae1830f79e17c137ac56c209aa6482d
mc-b <marcel.bernet@ch-open.ch>
2024-06-29 09:22:31 +0200

    FQDN nicht setzen

--- services/knative.sh
+++ services/knative.sh
@@ -8,7 +8,7 @@ sudo microk8s kubectl apply -f https://github.com/knative/serving/releases/downl
 sudo microk8s kubectl apply -f https://github.com/knative/serving/releases/download/knative-v1.14.1/serving-core.yaml    
 sudo microk8s kubectl apply -f https://github.com/knative/net-kourier/releases/download/knative-v1.14.0/kourier.yaml
 sudo microk8s kubectl patch configmap/config-network --namespace knative-serving --type merge --patch '{"data":{"ingress-class":"kourier.ingress.networking.knative.dev"}}'
-sudo microk8s kubectl patch configmap/config-domain --namespace knative-serving --type merge --patch '{"data":{"microk8s.mshome.net":""}}'  
+# sudo microk8s kubectl patch configmap/config-domain --namespace knative-serving --type merge --patch '{"data":{"microk8s.mshome.net":""}}'  
 
 # Eventing
 sudo microk8s kubectl apply -f https://github.com/knative/eventing/releases/download/knative-v1.14.2/eventing-crds.yaml
