8acb4f450c6aae3417987947ac239d3fe321b208
mc-b <marcel.bernet@ch-open.ch>
2024-07-02 19:31:43 +0200

    inkl. kafka

--- services/knative.sh
+++ services/knative.sh
@@ -20,6 +20,14 @@ sudo microk8s kubectl apply -f https://github.com/knative/eventing/releases/down
 # InMemory Broker
 sudo microk8s kubectl apply -f https://github.com/knative/eventing/releases/download/knative-v1.14.4/mt-channel-broker.yaml
 
+# Kafka Channel
+sudo microk8s kubectl apply -f https://github.com/knative-extensions/eventing-kafka-broker/releases/download/knative-v1.14.5/eventing-kafka-controller.yaml
+sudo microk8s kubectl apply -f https://github.com/knative-extensions/eventing-kafka-broker/releases/download/knative-v1.14.5/eventing-kafka-channel.yaml
+# Kafka Broker
+sudo microk8s kubectl apply -f https://github.com/knative-extensions/eventing-kafka-broker/releases/download/knative-v1.14.6/eventing-kafka-controller.yaml
+sudo microk8s kubectl apply -f https://github.com/knative-extensions/eventing-kafka-broker/releases/download/knative-v1.14.6/eventing-kafka-broker.yaml
+sudo microk8s kubectl apply -f https://github.com/knative-extensions/eventing-kafka-broker/releases/download/knative-v1.14.6/eventing-kafka-sink.yaml
+
 cat <<EOF | sudo microk8s kubectl apply -f -
 apiVersion: v1
 kind: ConfigMap
@@ -34,6 +42,18 @@ data:
       apiVersion: v1
       kind: ConfigMap
       name: imc-channel
+      namespace: knative-eventing
+    # This allows you to specify different defaults per-namespace,
+    # in this case the "some-namespace" namespace will use the Kafka
+    # channel ConfigMap by default (only for example, you will need
+    # to install kafka also to make use of this).
+    namespaceDefaults:
+      some-namespace:
+        brokerClass: MTChannelBasedBroker
+        apiVersion: v1
+        kind: ConfigMap
+        name: kafka-channel
+        namespace: knative-eventing
 ---
 apiVersion: v1
 kind: ConfigMap
@@ -44,15 +64,20 @@ data:
   channel-template-spec: |
     apiVersion: messaging.knative.dev/v1
     kind: InMemoryChannel
+---
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: kafka-channel
+  namespace: knative-eventing
+data:
+  channel-template-spec: |
+    apiVersion: messaging.knative.dev/v1alpha1
+    kind: KafkaChannel
+    spec:
+      numPartitions: 3
+      replicationFactor: 1        
 EOF
-
-# Kafka Channel
-# sudo microk8s kubectl apply -f https://github.com/knative-extensions/eventing-kafka-broker/releases/download/knative-v1.14.5/eventing-kafka-controller.yaml
-# sudo microk8s kubectl apply -f https://github.com/knative-extensions/eventing-kafka-broker/releases/download/knative-v1.14.5/eventing-kafka-channel.yaml
-# Kafka Broker
-# sudo microk8s kubectl delete -f https://github.com/knative-extensions/eventing-kafka-broker/releases/download/knative-v1.14.6/eventing-kafka-controller.yaml
-# sudo microk8s kubectl delete -f https://github.com/knative-extensions/eventing-kafka-broker/releases/download/knative-v1.14.6/eventing-kafka-broker.yaml
-# sudo microk8s kubectl delete -f https://github.com/knative-extensions/eventing-kafka-broker/releases/download/knative-v1.14.6/eventing-kafka-sink.yaml
 
 # CLI
 sudo curl -o /usr/local/bin/kn -sL https://github.com/knative/client/releases/download/knative-v1.14.0/kn-linux-amd64
