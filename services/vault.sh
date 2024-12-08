717e4199be2c32b0f7df21694cd66e4476870723
mc-b <marcel.bernet@mc-b.ch>
2024-12-08 13:58:52 +0100

    HashiCorp Vault (Security Storage)

---
+++ services/vault.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#
+#   Installiert und aktiviert eine HashiCorp Vault (Security Storage).
+#
+
+trap '' 1 3 9
+
+# Installation
+sudo apt-get -y update
+wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install vault
+
+# Einrichten als Service
+cat <<EOF | sudo tee /etc/systemd/system/vault-dev.service
+[Unit]
+Description=HashiCorp Vault Development Server
+After=network-online.target
+
+[Service]
+Type=simple
+WorkingDirectory=/home/ubuntu
+User=ubuntu
+Group=ubuntu
+Environment="VAULT_DEV_ROOT_TOKEN_ID=insecure"
+Environment="VAULT_UI_ENABLED=true"
+Environment="VAULT_DEV_LISTEN_ADDRESS=0.0.0.0:8200"
+ExecStart=/usr/bin/vault server -dev
+ExecReload=/bin/kill -HUP \$MAINPID
+KillMode=process
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Start Service
+sudo systemctl daemon-reload
+sudo systemctl start vault-dev.service
