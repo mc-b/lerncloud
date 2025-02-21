b578419afd1cae9b1d81bc0fe94422150c92c2c0
mc-b <marcel.bernet@mc-b.ch>
2025-02-21 10:00:59 +0100

    SAN deaktivieren, iSCSI aktivieren

---
+++ services/storage-patch.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#   
+#   Installation von iSCSI und deaktivieren von multipathd - wegen Probleme mit K8s bzw. longhorn
+#   nur Verwenden, wenn kein SAN gebraucht wird.
+
+# SAN Unterstuetzung
+sudo systemctl stop multipathd
+sudo systemctl disable multipathd
+sudo systemctl mask multipathd
+
+# iSCSI fuer longhorn.io
+
+sudo apt-get install open-iscsi
+sudo modprobe iscsi_tcp
+lsmod | grep iscsi_tcp
+echo "iscsi_tcp" | sudo tee /etc/modules-load.d/iscsi.conf
+
