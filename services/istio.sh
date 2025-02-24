8e0d9099a8ba0658c91b2c0cd862d1e3857a790f
Marcel Bernet <marcel.bernet@tbz.ch>
2025-02-24 20:20:42 +0100

    V1.21.0

--- services/istio.sh
+++ services/istio.sh
@@ -5,7 +5,8 @@
 
 # gibt Probleme mit Jaeger
 # export ISTIO_VERSION=1.24.2
-export ISTIO_VERSION=1.13.4
+#export ISTIO_VERSION=1.13.4
+export ISTIO_VERSION=1.21.0
 
 curl -L https://istio.io/downloadIstio | sh -
 sudo cp istio-${ISTIO_VERSION}/bin/istioctl /usr/local/bin/
