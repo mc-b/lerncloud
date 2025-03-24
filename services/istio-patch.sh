13ce56b25fd2872fadb30782bac8171cc709d9d9
Marcel Bernet <marcel.bernet@tbz.ch>
2025-03-24 17:25:05 +0100

    Memory Bedarf verkleinern

---
+++ services/istio-patch.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#   
+#   Istio - Patch. Verringert den Memory Bedarf von Istio Sidecars etc.
+#
+
+kubectl patch configmap istio-sidecar-injector -n istio-system \
+  --type merge \
+  -p '{
+    "data": {
+      "values": "{\"global\":{\"proxy\":{\"resources\":{\"limits\":{\"cpu\":\"50m\",\"memory\":\"128Mi\"},\"requests\":{\"cpu\":\"25m\",\"memory\":\"96Mi\"}}},\"waypoint\":{\"resources\":{\"limits\":{\"cpu\":\"50m\",\"memory\":\"128Mi\"},\"requests\":{\"cpu\":\"25m\",\"memory\":\"96Mi\"}}}}}"
+    }
+  }'
+
+# Kontrolle der Werte
+kubectl get configmap istio-sidecar-injector -n istio-system -o json | jq '.data.values | fromjson | .global.proxy.resources, .global.waypoint.resources'
+
+# Istio Services fuer max. 50 Services mit Sidecar
+  
+kubectl patch deployment istiod -n istio-system \
+  -p '{"spec":{"template":{"spec":{"containers":[{"name":"discovery","resources":{"limits":{"cpu":"300m","memory":"512Mi"},"requests":{"cpu":"100m","memory":"256Mi"}}}]}}}}'
+  
+kubectl patch deployment istio-ingressgateway -n istio-system \
+  -p '{"spec":{"template":{"spec":{"containers":[{"name":"istio-proxy","resources":{"limits":{"cpu":"150m","memory":"256Mi"},"requests":{"cpu":"50m","memory":"96Mi"}}}]}}}}'
+
+  
