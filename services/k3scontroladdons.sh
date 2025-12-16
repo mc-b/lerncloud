50089d100cc6b54b9f5e92ecd4d588b29f9f2a3f
mc-b <marcel.bernet@mc-b.ch>
2025-12-16 11:47:49 +0100

    nginx auf Port 80 und 443

--- services/k3scontroladdons.sh
+++ services/k3scontroladdons.sh
@@ -8,8 +8,18 @@ export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 
 echo "- 🔧 [INFO] install nginx"
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/baremetal/deploy.yaml
+
 kubectl patch ingressclass nginx \
   -p '{"metadata":{"annotations":{"ingressclass.kubernetes.io/is-default-class":"true"}}}'
+  
+kubectl patch deployment ingress-nginx-controller \
+  -n ingress-nginx \
+  --type='json' \
+  -p='[
+    {"op":"add","path":"/spec/template/spec/containers/0/ports/0/hostPort","value":80},
+    {"op":"add","path":"/spec/template/spec/containers/0/ports/1/hostPort","value":443}
+  ]'
+  
 
 echo "- 🔧 [INFO] install dashboard"
 kubectl apply -f https://raw.githubusercontent.com/mc-b/lerncloud/master/addons/dashboard.yaml 
