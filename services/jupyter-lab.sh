56c63eccf28d59425ead9bab0e6d75e2a6071eaf
mc-b <marcel.bernet@mc-b.ch>
2026-03-28 19:38:25 +0100

    pydantic fuer Structured In/Output

--- services/jupyter-lab.sh
+++ services/jupyter-lab.sh
@@ -15,7 +15,7 @@ pip install jupyterlab
 # OpenAI API als separater Kernel (Chat)
 python3 -m venv ~/.ai
 source ~/.ai/bin/activate
-pip install openai 
+pip install openai pydantic
 pip install ipykernel requests
 pip install nbconvert
 python3 -m ipykernel install --user --name=ai --display-name "Python (ai)"
