1f35f0031406d14d32afecda0bf9fb6977f28e85
mc-b <marcel.bernet@mc-b.ch>
2026-04-22 20:39:25 +0200

    Moeglichkeit Libraries install zu ueberspringen

--- services/jupyter-lab.sh
+++ services/jupyter-lab.sh
@@ -12,33 +12,39 @@ pip install jupyterlab
 
 # Jupyter Libraries fuer AI
 
-# OpenAI API als separater Kernel (Chat)
-python3 -m venv ~/.ai
-source ~/.ai/bin/activate
-pip install openai pydantic
-pip install ipykernel requests
-pip install nbconvert
-python3 -m ipykernel install --user --name=ai --display-name "Python (ai)"
+if [ "$SKIP_LIBRARIES" = "true" ]
+then
+    echo "Installation Libraries überspringen"
+else
+    # OpenAI API als separater Kernel (Chat)
+    python3 -m venv ~/.ai
+    source ~/.ai/bin/activate
+    pip install openai pydantic
+    pip install ipykernel requests
+    pip install nbconvert
+    python3 -m ipykernel install --user --name=ai --display-name "Python (ai)"
 
-# Hugging Face
-python3 -m venv ~/.hf
-source ~/.hf/bin/activate
-pip install -U ipykernel ipywidgets datasets pyarrow huggingface_hub fsspec transformers accelerate sentence-transformers sentencepiece peft pypdf requests tqdm numpy einops
-python3 -m ipykernel install --user --name=rag --display-name "Python (hf)"
 
-# MCP
-python3 -m venv ~/.mcp
-source ~/.mcp/bin/activate
-pip install ipykernel mcp requests
-pip install openai
-python3 -m ipykernel install --user --name=mcp --display-name "Python (mcp)"
-
-# Dapr (Multi Agent)
-python3 -m venv ~/.dapr
-source ~/.dapr/bin/activate
-pip install openai-agents dapr dapr-ext-grpc
-pip install ipykernel
-python3 -m ipykernel install --user --name=dapr --display-name "Python (dapr)"
+    # Hugging Face
+    python3 -m venv ~/.hf
+    source ~/.hf/bin/activate
+    pip install -U ipykernel ipywidgets datasets pyarrow huggingface_hub fsspec transformers accelerate sentence-transformers sentencepiece peft pypdf requests tqdm numpy einops
+    python3 -m ipykernel install --user --name=rag --display-name "Python (hf)"
+    
+    # MCP
+    python3 -m venv ~/.mcp
+    source ~/.mcp/bin/activate
+    pip install ipykernel mcp requests
+    pip install openai
+    python3 -m ipykernel install --user --name=mcp --display-name "Python (mcp)"
+    
+    # Dapr (Multi Agent)
+    python3 -m venv ~/.dapr
+    source ~/.dapr/bin/activate
+    pip install openai-agents dapr dapr-ext-grpc
+    pip install ipykernel
+    python3 -m ipykernel install --user --name=dapr --display-name "Python (dapr)"
+fi
 
 # Jupyter Lab as Service
 cat <<%EOF% | sudo tee /etc/systemd/system/jupyterlab.service
@@ -74,17 +80,14 @@ UserKnownHostsFile /dev/null
 LogLevel error
 EOF
 
-# Mount Verzeichnis
-[ -d ~/data ] && { ln -s ~/data ~/work; } || { mkdir -p ~/work; sudo chown ubuntu:ubuntu ~/work; }
-
 # Public IP anhand Cloud Provider setzen, WireGuard ueberschreibt alle
-curl -fsSL https://raw.githubusercontent.com/mc-b/lerncloud/main/scripts/get-server-ip.sh | bash | tee ~/work/server-ip
+curl -fsSL https://raw.githubusercontent.com/mc-b/lerncloud/main/scripts/get-server-ip.sh | bash | tee ~/data/server-ip
 
 # Eindeutige UUID pro Installation fuer IoT
-echo "UUID=\"$(uuid)\"" >~/work/uuid.py
+echo "UUID=\"$(uuid)\"" >~/data/uuid.py
 
 # Umgebungsvariablen fuer K8s AI Server, OpenAI und Hugging Face 
-cat <<EOF > ~/work/env.py
+cat <<EOF > ~/data/env.py
 KUBECONFIG_AI=""
 OPENAI_API_KEY=""
 HF_TOKEN=""
